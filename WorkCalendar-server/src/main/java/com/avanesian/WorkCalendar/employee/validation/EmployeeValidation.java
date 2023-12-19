e2e6693dfb6f092830add793dbb9fc906d219e7b
avan-es <bagratavanesian@yandex.ru>
2023-12-20 02:34:07 +0300

    feat: Добавил методы в валидацию Employee

--- WorkCalendar-server/src/main/java/com/avanesian/WorkCalendar/employee/validation/EmployeeValidation.java
+++ WorkCalendar-server/src/main/java/com/avanesian/WorkCalendar/employee/validation/EmployeeValidation.java
@@ -2,12 +2,15 @@ package com.avanesian.WorkCalendar.employee.validation;
 
 import com.avanesian.WorkCalendar.employee.model.Employee;
 import com.avanesian.WorkCalendar.employee.repository.EmployeeRepository;
+import com.avanesian.WorkCalendar.exeptions.ModelConflictException;
 import com.avanesian.WorkCalendar.exeptions.ModelValidationException;
+import com.avanesian.WorkCalendar.exeptions.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 @Component("employeeValidation")
@@ -16,7 +19,7 @@ public class EmployeeValidation {
 
     @Autowired
     @Qualifier("dbEmployeeRepository")
-    private EmployeeRepository EmployeeRepository;
+    private EmployeeRepository employeeRepository;
 
     private Pattern emailPattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
             "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
@@ -27,8 +30,38 @@ public class EmployeeValidation {
 
     public void checkEmail(Employee employee) {
         if (!isEmailValid(employee.getEmail())) {
-            log.error(String.format("Пользователь не создан. Ошибка в адресе почты: %s.", employee.getEmail()));
+            log.error(String.format("Сотрудник не создан. Ошибка в адресе почты: %s.", employee.getEmail()));
             throw new ModelValidationException(String.format("Почтовый адрес '%s' не может быть использован.",
+                    employee.getEmail()));
+        }
+    }
+
+    public Employee getEmployeeIfPresent(String email) {
+        Employee employee = employeeRepository.findEmployeeByEmail(email);
+        if (employee == null) {
+            log.error(String.format("Сотрудник с почтой %s не существует.", email));
+            throw new NotFoundException(String.format("Сотрудник с почтой %d не найден.", email));
+        }
+        return employee;
+    }
+
+
+    public void emailValidationForExistUser(Employee employee) {
+        if (employee.getEmail() != null) {
+            checkEmail(employee);
+            if (employeeRepository.findEmployeeByEmail(employee.getEmail()) != null) {
+                Optional<Employee> employeeForUpdate = employeeRepository.findById(employee.getId());
+                if (!employeeForUpdate.get().getEmail().equals(employee.getEmail())) {
+                    isEmailBuse(employee);
+                }
+            }
+        }
+    }
+
+    private void isEmailBuse(Employee employee) {
+        if (employeeRepository.findEmployeeByEmail(employee.getEmail()) != null) {
+            log.error(String.format("Сотрудник не создан. Почта %s уже занята.", employee.getEmail()));
+            throw new ModelConflictException(String.format("Почтовый адрес '%s' уже занят.",
                     employee.getEmail()));
         }
     }
