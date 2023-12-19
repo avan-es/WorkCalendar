1160401b9cd84f05f0c8474acbd02fc251f7bbea
avan-es <bagratavanesian@yandex.ru>
2023-12-19 17:03:13 +0300

    Initial commit

---
+++ src/test/java/com/avanesian/WorkCalendar/WorkCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avanesian.WorkCalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
