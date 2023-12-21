6ecba12c886d67be7a275f48eaf2756c8372deca
avan-es <bagratavanesian@yandex.ru>
2023-12-21 22:09:49 +0300

    feat: Добавил метод для поиска графика работы сотрудника

--- WorkCalendar-server/src/main/java/com/avanesian/WorkCalendar/schedule/repository/ScheduleRepository.java
+++ WorkCalendar-server/src/main/java/com/avanesian/WorkCalendar/schedule/repository/ScheduleRepository.java
@@ -4,11 +4,14 @@ import com.avanesian.WorkCalendar.schedule.model.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Component("dbScheduleRepository")
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    List<Schedule> getSchedulesByEmployeeId (Long employeeId);
+    List<Schedule> getSchedulesByEmployeeIdAndDateScheduleBetween (Long employeeId, LocalDate from, LocalDate to);
+
+    Schedule findScheduleById (Long scheduleId);
 
 }
