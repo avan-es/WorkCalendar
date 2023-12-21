e7007605aad402fd6de6b7c691500bb57afdf154
avan-es <bagratavanesian@yandex.ru>
2023-12-21 22:08:55 +0300

    feat: Добавил маппер для Schedule

--- WorkCalendar-server/src/main/java/com/avanesian/WorkCalendar/schedule/model/ScheduleMapper.java
+++ WorkCalendar-server/src/main/java/com/avanesian/WorkCalendar/schedule/model/ScheduleMapper.java
@@ -1,7 +1,26 @@
 package com.avanesian.WorkCalendar.schedule.model;
 
+import com.avanesian.WorkCalendar.schedule.dto.ScheduleFullDTO;
+import com.avanesian.WorkCalendar.schedule.dto.ScheduleShortDTO;
+
+
 public enum ScheduleMapper {
     INSTANT;
 
-   //public
+   public ScheduleFullDTO scheduleToScheduleFullDTO(Schedule schedule) {
+       ScheduleFullDTO scheduleFullDTO = new ScheduleFullDTO();
+       scheduleFullDTO.setId(schedule.getId());
+       scheduleFullDTO.setEmployeeId(schedule.getEmployeeId());
+       scheduleFullDTO.setDayType(schedule.getDayType());
+       scheduleFullDTO.setDateSchedule(schedule.getDateSchedule());
+       return scheduleFullDTO;
+   }
+
+   public ScheduleShortDTO scheduleToScheduleShortDTO(Schedule schedule) {
+       ScheduleShortDTO scheduleShortDTO = new ScheduleShortDTO();
+       scheduleShortDTO.setId(schedule.getId());
+       scheduleShortDTO.setDayType(schedule.getDayType());
+       scheduleShortDTO.setDateSchedule(schedule.getDateSchedule());
+       return scheduleShortDTO;
+   }
 }
