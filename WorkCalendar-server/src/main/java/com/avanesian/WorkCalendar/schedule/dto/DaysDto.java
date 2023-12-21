81b8e00a1d849040d74fdafa62491878b314bb7d
avan-es <bagratavanesian@yandex.ru>
2023-12-21 22:04:15 +0300

    feat: Добавил DTO для типов дней

---
+++ WorkCalendar-server/src/main/java/com/avanesian/WorkCalendar/schedule/dto/DaysDto.java
@@ -0,0 +1,13 @@
+package com.avanesian.WorkCalendar.schedule.dto;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class DaysDto {
+
+    private Long id;
+    private String dayType;
+
+}
