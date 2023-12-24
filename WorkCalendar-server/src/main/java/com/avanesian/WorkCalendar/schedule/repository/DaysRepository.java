7eb31831607818e17135e7c4821945330cb5932e
avan-es <bagratavanesian@yandex.ru>
2023-12-24 23:50:39 +0300

    fix: Рефакторинг SQL запроса

--- WorkCalendar-server/src/main/java/com/avanesian/WorkCalendar/schedule/repository/DaysRepository.java
+++ WorkCalendar-server/src/main/java/com/avanesian/WorkCalendar/schedule/repository/DaysRepository.java
@@ -2,6 +2,7 @@ package com.avanesian.WorkCalendar.schedule.repository;
 
 import com.avanesian.WorkCalendar.schedule.model.Days;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,12 +10,18 @@ import java.util.List;
 @Component("dbDaysRepository")
 public interface DaysRepository extends JpaRepository<Days, Long> {
 
-    Days findDaysByDayType(String dayType);
+    @Query(value = "SELECT * " +
+            "FROM days " +
+            "WHERE UPPER(day_type) = UPPER(?1) " +
+            "LIMIT 1", nativeQuery = true)
+    Days getDayByDayTypeName(String dayType);
 
     void deleteById (Long dayTypeId);
 
     List<Days> findAll();
 
     Days findDaysById (Long id);
+
+    List<Days> findDaysByIdIn(List<Long> daysId);
 
 }
