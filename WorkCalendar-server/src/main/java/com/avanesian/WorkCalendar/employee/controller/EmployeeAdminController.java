49d388051daaea77bd83f4824e16e7ae9ac084ea
avan-es <bagratavanesian@yandex.ru>
2023-12-29 18:22:38 +0300

    fix: рефакторинг

--- WorkCalendar-server/src/main/java/com/avanesian/WorkCalendar/employee/controller/EmployeeAdminController.java
+++ WorkCalendar-server/src/main/java/com/avanesian/WorkCalendar/employee/controller/EmployeeAdminController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/admin/employee")
 @RequiredArgsConstructor
-public class AdminController {
+public class EmployeeAdminController {
 
     private final EmployeeService employeeService;
 
